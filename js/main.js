05633efd1674db63d58f6de920f466e8ec8329c7
National-student <sanjarshopulatov62@gmail.com>
2022-03-05 13:09:49 +0500

    Initial

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-// js file
+var array = [-3.25, 3, -7.2, -5.6, 7.8, 9.25, -25]
+
+// Shu sonlarni shaxlitlab musbat ko'rinishida qaytar
+// array = [3, 3, 7, 6, 8, 9, 25]
+
+var arrayResult = []
+
+for (var i = 0; i < array.length; i++) {
+     
+    var number = Math.round(array[i]);
+
+    var result = Math.abs(number);
+
+    arrayResult.push(result)
+}
+console.log(arrayResult);
